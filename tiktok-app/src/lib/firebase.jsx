e023c31fb6606985b1025cd71aadeb08bcee8757
Rabit Ebibi <33910297+rabit1977@users.noreply.github.com>
2023-02-18 22:08:41 +0100

    ... first commit

---
+++ tiktok-app/src/lib/firebase.jsx
@@ -0,0 +1,7 @@
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
---
+++ tiktok-app/src/main.jsx
@@ -0,0 +1,14 @@
+import '@draft-js-plugins/mention/lib/plugin.css';
+import AppProviders from './components/AppProviders';
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AppProviders>
+      <App />
+    </AppProviders>
+  </React.StrictMode>
+);
